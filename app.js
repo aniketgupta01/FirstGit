61a3c9f8ffa2d5f5c1d340448af0a9c42e0b4674
aniketgupta01 <aniketg2831@gmail.com>
2023-11-04 12:27:02 +0530

    changed styling

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-console.log('I love git')
-console.log('Hello')
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 4px black';
+let addItem = document.getElementById('add-items');
+addItem.style.fontWeight = '900';
+addItem.style.color='green';
