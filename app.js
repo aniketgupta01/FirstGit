743b9489f440e724b7ea599510c1292280f1ed00
aniketgupta01 <aniketg2831@gmail.com>
2023-11-04 15:48:43 +0530

    added color to list items

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ header.style.borderBottom = 'solid 4px black';
 let addItem = document.getElementById('add-items');
 addItem.style.fontWeight = '900';
 addItem.style.color='green';
+let items = document.getElementsByClassName('list-group-item');
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = '900'
+}
+items[2].style.backgroundColor='green'
+
