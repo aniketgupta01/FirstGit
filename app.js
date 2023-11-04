3f902e7d15ff964124e75844b6dd380f2d76a207
aniketgupta01 <aniketg2831@gmail.com>
2023-11-04 15:57:19 +0530

    added list item

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ let addItem = document.getElementById('add-items');
 addItem.style.fontWeight = '900';
 addItem.style.color='green';
 let items = document.getElementsByClassName('list-group-item');
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = '900'
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight = '900'
+//     items[i].style.backgroundColor = 'yellow'
+// }
 items[2].style.backgroundColor='green'
+
+
+let li = document.getElementsByTagName('li');
+li[4].style.backgroundColor='yellow'
+
 
