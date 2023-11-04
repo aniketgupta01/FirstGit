a9bd9e15a95fc481d950d03674cb49b89934e661
aniketgupta01 <aniketg2831@gmail.com>
2023-11-04 16:30:09 +0530

    used queryselectorall

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ header.style.borderBottom = 'solid 4px black';
 let addItem = document.getElementById('add-items');
 addItem.style.fontWeight = '900';
 addItem.style.color='green';
-let items = document.getElementsByClassName('list-group-item');
-// for(let i=0;i<items.length;i++){
-//     items[i].style.fontWeight = '900'
-//     items[i].style.backgroundColor = 'yellow'
-// }
-items[2].style.backgroundColor='green'
+// let items = document.getElementsByClassName('list-group-item');
+// // for(let i=0;i<items.length;i++){
+// //     items[i].style.fontWeight = '900'
+// //     items[i].style.backgroundColor = 'yellow'
+// // }
+// items[2].style.backgroundColor='green'
 
 
 let li = document.getElementsByTagName('li');
-li[4].style.backgroundColor='yellow'
+// li[4].style.backgroundColor='yellow'
+
+
+//QuerySelector
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+// thirdItem.style.display='none';
+
+let items = document.querySelectorAll('.list-group-item');
+items[1].style.color='green';
+
+for(let i=0;i<items.length;i=i+2){
+    items[i].style.backgroundColor='green'
+}
+
 
 
